8c81bf871296a0aefdcd65438e8ff76e6bd8897e
Omel <mail@omel.dev>
2024-04-18 21:12:01 +0200

    Create velocity.ts

---
+++ src/app/components/classes/velocity.ts
@@ -0,0 +1,25 @@
+export class Velocity {
+  private x: number;
+  private y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public setX(x: number): void {
+    this.x = x;
+  }
+
+  public setY(y: number): void {
+    this.y = y;
+  }
+
+  public getX(): number {
+    return this.x;
+  }
+
+  public getY(): number {
+    return this.y;
+  }
+}
