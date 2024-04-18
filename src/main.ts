c59514b7f4af885fe59b0a224dbbe42044538c0f
Omel <mail@omel.dev>
2024-04-18 19:36:08 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
-import {AppComponent} from "./app/app.component";
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(r => console.log(r));
