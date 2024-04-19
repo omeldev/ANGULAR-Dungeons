09d653e36f2cfc72f11d06e59619e9e18d32b375
Omel <mail@omel.dev>
2024-04-19 10:57:29 +0200

    added ** routin to GameComponent

--- src/app/modules/route/app.routes.ts
+++ src/app/modules/route/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {ErrorComponent} from "../../components/error/error.component";
+import {GameComponent} from "../../components/game/game.component";
 
 export const routes: Routes = [
-  {path: '**', component: ErrorComponent}
+  {path: '**', component: GameComponent}
 ];
