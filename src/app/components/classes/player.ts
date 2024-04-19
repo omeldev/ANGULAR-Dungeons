1f036dd337a69a85ab51e2e37352a2a66f0cca1d
Omel <mail@omel.dev>
2024-04-19 10:56:37 +0200

    Implemented Player Move

--- src/app/components/classes/player.ts
+++ src/app/components/classes/player.ts
@@ -1,11 +1,15 @@
 import {Position} from "./position";
 import {Velocity} from "./velocity";
 import {Sprite} from "./sprite";
+import {GameComponent} from "../game/game.component";
 
 export class Player {
   private readonly sprite: Sprite;
   private position: Position;
   private velocity: Velocity;
+
+  private width = 100;
+  private height = 100;
 
   constructor(position: Position, sprite: Sprite) {
     this.sprite = sprite;
@@ -19,6 +23,22 @@ export class Player {
 
   public getPosition(): Position {
     return this.position;
+  }
+
+  public getWidth(): number {
+    return this.width;
+  }
+
+  public getHeight(): number {
+    return this.height;
+  }
+
+  public setWidth(width: number): void {
+    this.width = width;
+  }
+
+  public setHeight(height: number): void {
+    this.height = height;
   }
 
   public getVelocity(): Velocity {
@@ -38,15 +58,25 @@ export class Player {
     this.position.setY(this.position.getY() + this.velocity.getY());
   }
 
-  public draw(): void {
-
+  public draw(context: CanvasRenderingContext2D): void {
+    context.fillStyle = 'black';
+    context.fillRect(this.getPosition().getX(), this.getPosition().getY(), this.getWidth(), this.getHeight());
   }
 
-  public update(): void {
+  public update(context: CanvasRenderingContext2D): void {
+
     this.move();
-    this.draw()
-  }
 
+    this.getVelocity().setY(1);
+    
+    if (this.getPosition().getY() + this.getHeight() >= GameComponent.canvasHeight) {
+      this.getVelocity().setY(0);
+    }
+
+    this.draw(context);
+
+
+  }
 
 
 }
