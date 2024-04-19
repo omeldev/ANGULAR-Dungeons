3af983ff44342e72ff1985c4370317a530c0eae5
Omel <mail@omel.dev>
2024-04-19 13:18:32 +0200

    added Keystroke functionality

---
+++ src/app/components/listener/keystroke.ts
@@ -0,0 +1,83 @@
+export class GameKey {
+  private readonly key: string;
+  private pressed: boolean;
+
+  constructor(key: string, pressed: boolean) {
+    this.key = key;
+    this.pressed = pressed;
+
+  }
+
+  public getKey(): string {
+    return this.key;
+  }
+
+  public isPressed(): boolean {
+    return this.pressed;
+  }
+
+  public setPressed(pressed: boolean): void {
+    this.pressed = pressed;
+  }
+
+
+}
+
+export const KEYS: GameKey[] = [
+  new GameKey('w', false),
+  new GameKey('a', false),
+  new GameKey('d', false),
+];
+
+export function setKeyPressed(key: string, pressed: boolean): void {
+  const gameKey = KEYS.find(gameKey => gameKey.getKey() === key);
+  gameKey!.setPressed(pressed);
+}
+
+export function isKeyPressed(key: string): boolean {
+  const gameKey = KEYS.find(gameKey => gameKey.getKey() === key);
+  return gameKey!.isPressed();
+}
+export function registerKeystrokes(): void {
+
+  window.addEventListener('keydown', (event) => {
+    const key = event.key;
+
+    switch (key) {
+      case 'w':
+        setKeyPressed('w', true);
+       break;
+      case 'a':
+        setKeyPressed('a', true);
+        break;
+      case 'd':
+        setKeyPressed('d', true);
+        break;
+
+        default:
+          break;
+    }
+  });
+
+  window.addEventListener('keyup', (event) => {
+    const key = event.key;
+
+    switch (key) {
+      case 'w':
+        setKeyPressed('w', false);
+        break;
+      case 'a':
+        setKeyPressed('a', false);
+        break;
+      case 'd':
+        setKeyPressed('d', false);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+}
+
+
