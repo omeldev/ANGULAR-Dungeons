c35a057ee613422b92167e5968d843620f3118b3
Omel <mail@omel.dev>
2024-04-20 20:10:18 +0200

    Cleanup Position

--- src/app/classes/position.ts
+++ src/app/classes/position.ts
@@ -1,11 +1,9 @@
 export class Position {
-  private x: number;
-  private y: number;
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
+  constructor(
+    private x: number,
+    private y: number
+  ) { }
 
   public setX(x: number): void {
     this.x = x;
