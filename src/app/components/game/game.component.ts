4b61beb94facf3c2a4ba669f8b1e4b80b98b835e
Omel <mail@omel.dev>
2024-04-20 18:55:04 +0200

    implemented colissionDraw for level 1

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -4,6 +4,8 @@ import {Sprite} from "../../classes/sprite";
 import {Position} from "../../classes/position";
 import {registerKeystrokes} from "../../listener/keystroke";
 import {Animation} from "../../classes/animation";
+import {level1} from "../../levels/levels";
+import {CollisionBlock} from "../../classes/collision/CollisionBlock";
 
 @Component({
   selector: 'app-game',
@@ -18,14 +20,14 @@ export class GameComponent implements AfterViewInit {
   public context: CanvasRenderingContext2D | undefined;
   private player: Player;
 
-
   constructor() {
     const spr = new Sprite('../../../assets/sprites/player/guard_1.png', new Position(100, 100));
     spr.setIsBackground(true);
     this.player = new Player(new Position(100, 100), spr);
 
-  }
 
+
+  }
   ngAfterViewInit(): void {
     this.context = this.canvas?.nativeElement.getContext('2d')!;
     this.iniCanvas();
@@ -42,12 +44,11 @@ export class GameComponent implements AfterViewInit {
 
   private animate() {
     window.requestAnimationFrame(() => this.animate());
-    this.context!.fillStyle = 'white';
-    this.context!.fillRect(0, 0, GameComponent.canvasWidth, GameComponent.canvasHeight);
-
+    level1.draw(this.context!);
+    //TODO JUST FOR TESTING
+    level1.drawCollisionBlocks(this.context!);
+    //TODO JUST FOR TESTING
     this.player.update(this.context!);
-
-
   }
 
 
