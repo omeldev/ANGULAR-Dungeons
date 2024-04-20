b7eb3fecd88d92f2429ac26f095982454bda9517
Omel <mail@omel.dev>
2024-04-20 21:12:00 +0200

    added experimental NOT working collision

--- src/app/classes/collision/CollisionBlock.ts
+++ src/app/classes/collision/CollisionBlock.ts
@@ -1,4 +1,6 @@
 import {Position} from "../position";
+import {Sprite} from "../sprite";
+import {Player} from "../player";
 
 export class CollisionBlock {
   private position: Position;
@@ -15,8 +17,53 @@ export class CollisionBlock {
     }
   }
 
+  public collidesWith(pl: Player) {
+    const x = pl.getSprite().getX(), y = pl.getSprite().getY();
+    const width = pl.getWidth(), height = pl.getHeight();
+    if (x + width > this.position.getX() && x < this.position.getX() + this.width){
+      console.log("collided")
+
+      return true;
+    }
+    if (y + height > this.position.getY() && y < this.position.getY() + this.height){
+      console.log("collided")
+      return true;
+    }
+    return false;
+  }
+
+  public collidesHorizontallyWith(pl: Player) {
+    if (
+      pl.getPosition().getX() <=
+      this.position.getX() + this.width &&
+      pl.getPosition().getX() + this.width >=
+      this.position.getX() &&
+      pl.getPosition().getY() + this.height >=
+      this.position.getY() &&
+      pl.getPosition().getY() <=
+      this.position.getY() + this.height
+    ) {
+      // collision on x axis going to the left
+      pl.getVelocity().setY(0);
+    }
+
+  }
+
+  public getPosition(): Position {
+    return this.position;
+  }
+
   public draw(context: CanvasRenderingContext2D): void {
     context.fillStyle = "rgba(240, 52, 52, 0.3)";
     context.fillRect(this.position.getX(), this.position.getY(), this.width, this.height);
+  }
+
+  public getWidth(): number {
+    return this.width;
+  }
+
+  public getHeight(): number {
+    return this.height;
+
   }
 }
