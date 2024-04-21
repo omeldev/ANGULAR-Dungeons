68ebad528e4af30001f735d961d273551658a399
Omel <mail@omel.dev>
2024-04-21 10:45:09 +0200

    deleted experimental code

--- src/app/classes/collision/CollisionBlock.ts
+++ src/app/classes/collision/CollisionBlock.ts
@@ -16,37 +16,6 @@ export class CollisionBlock {
     }
   }
 
-  public collidesWith(pl: Player) {
-    const x = pl.getSprite().getX(), y = pl.getSprite().getY();
-    const width = pl.getWidth(), height = pl.getHeight();
-    if (x + width > this.position.getX() && x < this.position.getX() + this.width) {
-      console.log("collided")
-
-      return true;
-    }
-    if (y + height > this.position.getY() && y < this.position.getY() + this.height) {
-      console.log("collided")
-      return true;
-    }
-    return false;
-  }
-
-  public collidesHorizontallyWith(pl: Player) {
-    if (
-      pl.getPosition().getX() <=
-      this.position.getX() + this.width &&
-      pl.getPosition().getX() + this.width >=
-      this.position.getX() &&
-      pl.getPosition().getY() + this.height >=
-      this.position.getY() &&
-      pl.getPosition().getY() <=
-      this.position.getY() + this.height
-    ) {
-      // collision on x axis going to the left
-      pl.getVelocity().setY(0);
-    }
-
-  }
 
   public getPosition(): Position {
     return this.position;
