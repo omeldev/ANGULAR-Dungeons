fbb2b795f8cf7096ac07b59f139576f839c4d439
Omel <mail@omel.dev>
2024-04-22 10:58:19 +0200

    Update app.routes.ts

--- src/app/modules/route/app.routes.ts
+++ src/app/modules/route/app.routes.ts
@@ -2,5 +2,6 @@ import {Routes} from '@angular/router';
 import {GameComponent} from "../../components/game/game.component";
 
 export const routes: Routes = [
+
   {path: '**', component: GameComponent}
 ];
