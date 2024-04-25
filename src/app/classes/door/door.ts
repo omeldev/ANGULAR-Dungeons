b1907065246f83c27a2c59321a300476c2ad3a52
Omel <mail@omel.dev>
2024-04-25 19:03:38 +0200

    Made Sprite superclass of door

--- src/app/classes/door/door.ts
+++ src/app/classes/door/door.ts
@@ -2,25 +2,13 @@ import {Position} from "../entitiy/position";
 import {Sprite} from "../entitiy/sprite";
 import {Player} from "../entitiy/player/player";
 
-export class Door {
-  private position: Position;
+export class Door extends Sprite {
   private isFinalDoor: boolean;
   private destination: Door;
-  private sprite: Sprite;
-
   constructor(position: Position, isFinalDoor?: boolean, destination?: Door) {
-    this.position = position;
+    super('../../../assets/sprites/door/door.png', position);
     this.isFinalDoor = false;
     this.destination = this;
-    this.sprite = new Sprite('../../../assets/sprites/door/door.png', position);
-  }
-
-  public getPosition(): Position {
-    return this.position;
-  }
-
-  public setPosition(position: Position): void {
-    this.position = position;
   }
 
   public getIsFinalDoor(): boolean {
@@ -39,15 +27,16 @@ export class Door {
     this.destination = destination;
   }
 
-  public draw(context: CanvasRenderingContext2D): void {
-    this.sprite.drawSprite(context);
-  }
 
+  /**
+   * Check if the player collides with the door
+   * @param player
+   */
   public checkCollision(player: Player): boolean {
-    return this.sprite.getPosition().getX() < player.getPosition().getX() + player.getSprite().getWidth() &&
-      this.sprite.getPosition().getX() + this.sprite.getWidth() > player.getPosition().getX() &&
-      this.sprite.getPosition().getY() < player.getPosition().getY() + player.getSprite().getHeight() &&
-      this.sprite.getPosition().getY() + this.sprite.getHeight() > player.getPosition().getY();
+    return this.getPosition().getX() < player.getPosition().getX() + player.getSprite().getWidth() &&
+      this.getPosition().getX() + this.getWidth() > player.getPosition().getX() &&
+      this.getPosition().getY() < player.getPosition().getY() + player.getSprite().getHeight() &&
+      this.getPosition().getY() + this.getHeight() > player.getPosition().getY();
 
   }
 
