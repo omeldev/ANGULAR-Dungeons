0f1c011f0c492a02460cd83f5c953d7074c5f81f
Omel <mail@omel.dev>
2024-04-25 17:09:29 +0200

    Cleanup code -> Sprite is now a Super Class

--- src/app/classes/entitiy/sprite.ts
+++ src/app/classes/entitiy/sprite.ts
@@ -2,11 +2,9 @@ import {Position} from "./position";
 import {Scale} from "./scale";
 
 export class Sprite {
-  private imageSrc: string;
+  private readonly position: Position;
   private readonly image: HTMLImageElement;
-  private dirty: boolean;
-  private position: Position;
-  private isBackground: boolean;
+  private readonly imageSrc: string;
 
   private width: number = 0;
   private height: number = 0;
@@ -22,24 +20,8 @@ export class Sprite {
       this.width = this.image.width;
       this.height = this.image.height;
     }
-
     this.image.src = this.imageSrc;
-    this.dirty = true;
-    this.isBackground = true;
-
     this.scale = new Scale(1);
-
-
-  }
-
-  public getImageSrc(): string {
-    return this.imageSrc;
-  }
-
-  public setImageSrc(imageSrc: string): void {
-    this.imageSrc = imageSrc;
-    this.image.src = this.imageSrc;
-    this.dirty = true;
   }
 
   public getImage(): HTMLImageElement {
@@ -51,33 +33,14 @@ export class Sprite {
   }
 
   public setPosition(position: Position): void {
-    this.position = position;
-    this.dirty = true;
+    this.getPosition().setY(position.getY());
+    this.getPosition().setX(position.getX());
   }
 
   public setScale(scale: Scale): void {
     this.scale = scale;
     this.height = this.image.height * this.scale.getScale();
     this.width = this.image.width * this.scale.getScale();
-    this.dirty = true;
-  }
-
-  public setX(x: number): void {
-    this.position.setX(x);
-    this.dirty = true;
-  }
-
-  public setY(y: number): void {
-    this.position.setY(y);
-    this.dirty = true;
-  }
-
-  public getX(): number {
-    return this.position.getX();
-  }
-
-  public getY(): number {
-    return this.position.getY();
   }
 
   public getWidth(): number {
@@ -88,51 +51,12 @@ export class Sprite {
     return this.height * this.getScale().getScale();
   }
 
-  public isDirty(): boolean {
-    return this.dirty;
-  }
-
-  public setWidth(width: number): void {
-    this.width = width;
-    this.dirty = true;
-  }
-
-  public setHeight(height: number): void {
-    this.height = height;
-    this.dirty = true;
-  }
-
-  public setIsBackground(isBackground: boolean): void {
-    this.isBackground = isBackground;
-  }
-
-  public getIsBackground(): boolean {
-    return this.isBackground;
-  }
-
-
   public getScale(): Scale {
     return this.scale;
   }
 
-  public draw(context: CanvasRenderingContext2D): void {
-    if (this.isBackground) {
+  public drawSprite(context: CanvasRenderingContext2D): void {
       context.drawImage(this.image, this.position.getX(), this.position.getY(), this.getWidth(), this.getHeight());
-      return;
-    }
-
-    if (this.isDirty()) {
-      context.drawImage(this.image, this.position.getX(), this.position.getY(), this.getWidth(), this.getHeight());
-      this.setDirty(false);
-    }
   }
 
-  public update(context: CanvasRenderingContext2D, position: Position) {
-    this.setPosition(position);
-    this.draw(context);
-  }
-
-  private setDirty(b: boolean) {
-    this.dirty = b;
-  }
 }
