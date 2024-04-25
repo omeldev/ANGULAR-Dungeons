3b9a44a0c1715c3effadefec54d5dab33bbecceb
Omel <mail@omel.dev>
2024-04-25 19:11:27 +0200

    Added Documentation

--- src/app/classes/level/level.ts
+++ src/app/classes/level/level.ts
@@ -11,6 +11,14 @@ export class Level extends Sprite {
   private readonly finalDoor: Door;
 
 
+  /**
+   * Create a new Level
+   * @param background {Sprite} of the Level
+   * @param spawnPoint {Position} of the Player spawn point
+   * @param collisions {number[]} of the Level (Made with Tiled)
+   * @param finalDoor {Door} of the Level
+   * @param otherDoors {Door[]} of the Level
+   */
   constructor(background: Sprite, spawnPoint: Position, collisions: number[], finalDoor?: Door, otherDoors?: Door[]) {
     super(background.getImage().src, background.getPosition());
     this.collisions = collisions;
@@ -18,19 +26,29 @@ export class Level extends Sprite {
     this.finalDoor = finalDoor ? finalDoor : new Door(new Position(13 * 64, 64 + 16), true, new Door(new Position(0, 0), true));
 
     this.spawnPoint = spawnPoint;
+
+
+    /**
+     * Convert the collisions array to CollisionBlocks
+     * 292 is the symbol for a collision block
+     */
     this.getCollisionsMap().forEach((row, y) => {
       row.forEach((symbol, x) => {
         if (symbol === 292) {
           const block = new CollisionBlock(new Position(x * 64, y * 64));
           this.collisionBlocks.push(block);
-          console.log("collision block added")
-          console.log(block.getPosition().getX(), block.getPosition().getY())
         }
       })
     });
 
   }
 
+  /**
+   * Get the collisions of the Level
+   * @returns {number[][]} of the Level
+   * Transform the 1D array to a 2D array
+   * TODO: Independent from the size of the Level
+   */
   public getCollisionsMap(): number[][] {
     const rows: number[][] = [];
 
@@ -42,28 +60,51 @@ export class Level extends Sprite {
     return rows;
   }
 
+  /**
+   * Get the spawn point of the Level
+   * @returns {Position} of the spawn point
+   */
   public getSpawnPoint(): Position {
     return this.spawnPoint;
   }
 
+  /**
+   * Set the spawn point of the Level
+   * @param spawnPoint {Position} of the spawn point
+   */
   public setSpawnPoint(spawnPoint: Position): void {
     this.getSpawnPoint().setX(spawnPoint.getX());
     this.getSpawnPoint().setY(spawnPoint.getY());
   }
 
+  /**
+   * Draw the collision blocks. For debugging purposes
+   * @param context {CanvasRenderingContext2D} of the Canvas
+   */
   public drawCollisionBlocks(context: CanvasRenderingContext2D): void {
-
     this.collisionBlocks.forEach(block => block.draw(context!));
   }
 
+  /**
+   * Get the collision blocks of the Level
+   * @returns {CollisionBlock[]} of the Level
+   */
   public getCollisionBlocks(): CollisionBlock[] {
     return this.collisionBlocks;
   }
 
+  /**
+   * Get the background of the Level
+   * @returns {Sprite} of the Level
+   */
   public getBackground(): Sprite {
     return this.background;
   }
 
+  /**
+   * Get the final door of the Level
+   * @returns {Door} of the Level
+   */
   public getFinalDoor(): Door {
     return this.finalDoor;
   }
