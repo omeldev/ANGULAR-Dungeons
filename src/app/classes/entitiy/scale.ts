9b785a358850dae9f43fbe55f41b624d44322277
Omel <mail@omel.dev>
2024-04-25 18:34:44 +0200

    Added Documentation

--- src/app/classes/entitiy/scale.ts
+++ src/app/classes/entitiy/scale.ts
@@ -1,13 +1,25 @@
 export class Scale {
 
 
+  /**
+   * Create a new scale
+   * @param scale {number} size of the Scaling 1.0 = 100%
+   */
   constructor(private scale: number) {
-
   }
 
+  /**
+   * Set the scale
+   * @param scale {number} size of the Scaling e.g. 1.0 = 100%
+   */
   public setScale(scale: number): void {
     this.scale = scale;
   }
+
+  /**
+   * Get the scale
+   * @returns {number} size of the Scaling as double
+   */
 
   public getScale(): number {
     return this.scale;
