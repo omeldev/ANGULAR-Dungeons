5f4467aaf7ec13e033c0d917a83b4ceb6fc2c9f9
Omel <mail@omel.dev>
2024-04-25 21:00:54 +0200

    Implemented Coins into Level class

--- src/app/classes/level/level.ts
+++ src/app/classes/level/level.ts
@@ -2,11 +2,13 @@ import {CollisionBlock} from "../collision/CollisionBlock";
 import {Position} from "../entitiy/position";
 import {Sprite} from "../entitiy/sprite";
 import {Door} from "../door/door";
+import {Coin} from "../coin/coin";
 
 export class Level extends Sprite {
   private collisions: number[];
   private readonly background: Sprite;
   private collisionBlocks: CollisionBlock[] = [];
+  private readonly coins: Coin[] = [];
   private readonly spawnPoint: Position;
   private readonly finalDoor: Door;
 
@@ -18,9 +20,25 @@ export class Level extends Sprite {
    * @param collisions {number[]} of the Level (Made with Tiled)
    * @param finalDoor {Door} of the Level
    * @param otherDoors {Door[]} of the Level
+   * @param coins
    */
-  constructor(background: Sprite, spawnPoint: Position, collisions: number[], finalDoor?: Door, otherDoors?: Door[]) {
-    super(background.getImage().src, background.getPosition());
+  constructor(background: Sprite, spawnPoint: Position, collisions: number[], finalDoor?: Door, coins?: number[]) {
+    super(background.getImage().src, background.getPosition(), () => {
+      this.getCollisionsMap().forEach((row, y) => {
+        row.forEach((symbol, x) => {
+          if (symbol === 292) {
+            const block = new CollisionBlock(new Position(x * 64, y * 64));
+            this.collisionBlocks.push(block);
+          }
+        })
+      });
+
+
+      if(coins){
+        this.setCoins(coins);
+      }
+
+    });
     this.collisions = collisions;
     this.background = background;
     this.finalDoor = finalDoor ? finalDoor : new Door(new Position(13 * 64, 64 + 16), true, new Door(new Position(0, 0), true));
@@ -32,14 +50,28 @@ export class Level extends Sprite {
      * Convert the collisions array to CollisionBlocks
      * 292 is the symbol for a collision block
      */
-    this.getCollisionsMap().forEach((row, y) => {
+
+  }
+
+  public setCoins(coins: number[]){
+    const rows: number[][] = [];
+    const rowSize = this.getWidth() / 64;
+    if (this.getWidth() % 64 !== 0) throw new Error("invalid width");
+
+
+    for (let i = 0; i < coins.length; i += rowSize) {
+      rows.push(coins.slice(i, rowSize + i));
+    }
+
+    rows.forEach(((row, y) => {
       row.forEach((symbol, x) => {
-        if (symbol === 292) {
-          const block = new CollisionBlock(new Position(x * 64, y * 64));
-          this.collisionBlocks.push(block);
+        if (symbol === 293) {
+          const coin = new Coin(new Position(x * 64, (y + 1.5) * 64));
+
+          this.coins.push(coin);
         }
-      })
-    });
+      });
+    }));
 
   }
 
@@ -52,9 +84,14 @@ export class Level extends Sprite {
   public getCollisionsMap(): number[][] {
     const rows: number[][] = [];
 
-    const chunkSize = this.getBackground().getWidth() / 64;
-    for (let i = 0; i < this.collisions.length; i += 16) {
-      rows.push(this.collisions.slice(i, 16 + i));
+    const rowSize = this.getWidth() / 64;
+
+    if (this.getWidth() % 64 !== 0) throw new Error("invalid width");
+
+    console.log(rowSize);
+
+    for (let i = 0; i < this.collisions.length; i += rowSize) {
+      rows.push(this.collisions.slice(i, rowSize + i));
     }
 
     return rows;
@@ -91,6 +128,14 @@ export class Level extends Sprite {
    */
   public getCollisionBlocks(): CollisionBlock[] {
     return this.collisionBlocks;
+  }
+
+  public getCoins(): Coin[] {
+    return this.coins;
+  }
+
+  public drawCoins(context: CanvasRenderingContext2D): void {
+    this.coins.forEach(coin => coin.drawSprite(context));
   }
 
   /**
