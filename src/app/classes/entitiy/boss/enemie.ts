a1edfd72ebaaf0143c700e7756ac17ea660e9b85
Omel <mail@omel.dev>
2024-04-30 23:06:58 +0200

    Implemented Healthbar

---
+++ src/app/classes/entitiy/boss/enemie.ts
@@ -0,0 +1,60 @@
+import {Hitbox} from "../../collision/hitbox";
+import {Direction, Gizmo} from "../gizmo/gizmo";
+import {Position} from "../position";
+import {Player} from "../player/player";
+
+export type Animation = {
+  [key: string]: {
+    frameRate: number;
+    frameBuffer: number;
+    loop: boolean;
+    imageSrc: string;
+    onComplete?: () => void;
+  }
+}
+
+export abstract class Enemie extends Gizmo {
+  private attackBox: Hitbox;
+  public isAttacking: boolean = false;
+  public attackCooldown: number = 0;
+  constructor(attackBox: Hitbox, spriteSrc: string, position: Position, animations: Animation, frameRate: number = 6, hitbox: Hitbox = new Hitbox(position, 20, 20)) {
+    super(spriteSrc, position, animations, frameRate, hitbox);
+    this.attackBox = attackBox;
+  }
+
+  public getAttackBox(): Hitbox {
+    return this.attackBox;
+  }
+
+
+  public override updateHitbox(offsetX: number, offsetY: number) {
+    super.updateHitbox(offsetX, offsetY);
+    if (this.lastDirection === Direction.LEFT) {
+      this.attackBox.getPosition().setX(this.getPosition().getX() + this.attackBox.getWidth() + 5);
+    } else {
+      this.attackBox.getPosition().setX(this.getPosition().getX() + 140);
+    }
+    this.attackBox.getPosition().setY(this.getPosition().getY() + offsetY);
+
+  }
+
+  public override drawSprite(context: CanvasRenderingContext2D, delta: number) {
+    super.drawSprite(context, delta);
+  }
+
+
+  onCollide(context: CanvasRenderingContext2D, delta: number): void {
+  }
+
+  onSwitch(context: CanvasRenderingContext2D, delta: number): void {
+  }
+
+  abstract attackBoxCollide(player: Player): void;
+
+  public override collidesWithPlayer(player: Player): boolean {
+    if (this.attackBox.collidesWith(player.getHitbox())) {
+      if (this.isAttacking) this.attackBoxCollide(player);
+    }
+    return super.collidesWithPlayer(player);
+  }
+}
---
+++ src/app/classes/gui/bar/healthbar.ts
@@ -0,0 +1,63 @@
+import {Position} from "../../entitiy/position";
+import {GameComponent} from "../../../components/game/game.component";
+import {Scale} from "../../entitiy/scale";
+
+type Heart = 'full' | 'half' | 'empty';
+
+export class Healthbar {
+  private fullHeart: HTMLImageElement;
+  private halfHeart: HTMLImageElement;
+  private emptyHeart: HTMLImageElement;
+  position: Position;
+  private Scale: Scale = new Scale(1);
+
+  constructor(position: Position) {
+    this.position = position;
+    this.fullHeart = new Image();
+    this.halfHeart = new Image();
+    this.emptyHeart = new Image();
+    this.fullHeart.src = '../../../assets/sprites/player/heart/fullHeart.png';
+    this.halfHeart.src = '../../../assets/sprites/player/heart/halfHeart.png';
+    this.emptyHeart.src = '../../../assets/sprites/player/heart/emptyHeart.png';
+
+    this.fullHeart.onload = () => {
+      this.fullHeart.width = this.fullHeart.width * this.Scale.getScale();
+      this.fullHeart.height = this.fullHeart.height * this.Scale.getScale();
+    }
+    this.halfHeart.onload = () => {
+      this.halfHeart.width = this.halfHeart.width * this.Scale.getScale();
+      this.halfHeart.height = this.halfHeart.height * this.Scale.getScale();
+    }
+
+    this.emptyHeart.onload = () => {
+      this.emptyHeart.width = this.emptyHeart.width * this.Scale.getScale();
+      this.emptyHeart.height = this.emptyHeart.height * this.Scale.getScale();
+    }
+  }
+
+  public calculateHealth(entityHealth: number, entityMaxHealth: number): Heart[] {
+    if (entityHealth > entityMaxHealth) throw new Error("invalid health");
+    const heartValue = 10;
+    const full = Math.floor(entityHealth / heartValue)
+    const half: boolean = entityHealth % heartValue >= heartValue / 2;
+
+    const hearts: Heart[] = new Array<Heart>(Math.ceil(entityMaxHealth / heartValue)).fill('empty');
+    if (entityHealth < 0) return hearts;
+    let counter = 0;
+    for (counter; counter < full; counter++) hearts[counter] = 'full'
+    if (half) hearts[counter + 1] = 'half';
+
+    return hearts;
+  }
+
+  public draw(context: CanvasRenderingContext2D, offset: number): void {
+    const hearts = this.calculateHealth(GameComponent.player.health, GameComponent.player.maxHealth);
+
+    hearts.forEach((heart, index) => {
+      if (heart === 'full') context.drawImage(this.fullHeart, this.position.getX() + index * offset, this.position.getY(), this.fullHeart.width, this.fullHeart.height);
+      else if (heart === 'half') context.drawImage(this.halfHeart, this.position.getX() + index * offset, this.position.getY(), this.halfHeart.width, this.halfHeart.height);
+      else context.drawImage(this.emptyHeart, this.position.getX() + index * offset, this.position.getY(), this.emptyHeart.width, this.emptyHeart.height);
+    });
+
+  }
+}
