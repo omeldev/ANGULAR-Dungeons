fba402b3e7bcf49a456c94fa252108416537df6d
Omel <mail@omel.dev>
2024-05-08 20:26:39 +0200

    Update potioneffect.ts

--- src/app/classes/collectibles/potion/potioneffect.ts
+++ src/app/classes/collectibles/potion/potioneffect.ts
@@ -2,10 +2,11 @@ export class PotionEffect {
 
   constructor(public type: PotionEffectType, public duration: number, public amplifier: number) {
 
+
   }
 
   public update(context: CanvasRenderingContext2D, delta: number): void {
-    this.duration--;
+    this.duration -= delta;
   }
 
 }
